87a6b06a2f923256b635aabd4398959006eef409
opoku1968 <kofipos@yahoo.com>
2024-01-09 17:08:56 +0000

    created and updated cmoment

---
+++ comments/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from rest_framework import serializers
+from .models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+    
+    
+    class Meta:
+       model = Comment
+       fields ='__all__'
+
+class CommentDetailSerializer(CommentSerializer):
+    post = serializers.ReadOnlyField(source='post.id')

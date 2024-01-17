40c853c7559e645de5f3e9f4ee809328ecbc60da
opoku1968 <kofipos@yahoo.com>
2024-01-17 20:03:23 +0000

    updated files

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/',include('rest_framework.urls')),
     path('',include('profiles.urls')),
-    path('dj-rest-auth/',include('dj_rest_auth.urls')),
+   
     path('dj-rest-auth/registration/',include('dj_rest_auth.registration.urls')),
     path('',include('posts.urls')),
     path('',include('comments.urls')),
